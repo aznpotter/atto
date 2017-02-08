eab398c95c9935961dc13c0b69e03205ae501a99
asdf <harry94lfms@gmail.com>
2017-02-08 11:02:04 +0900

    github instructions on test.py

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
 # this is git test
 # for branch1
+print("\n(1) git status \n(2) git add file_name\n(3) git commit -m quote_message_quote\n(4) git push origin directory_name\n")
+print("try http://product.hubspot.com/blog/git-and-github-tutorial-for-beginners\n")
