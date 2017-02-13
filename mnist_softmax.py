0d6de7fb7fa70eb06b27c05d9d78555571173308
asdf <harry94lfms@gmail.com>
2017-02-13 09:37:57 +0900

    cifar added

--- mnist_softmax.py
+++ mnist_softmax.py
@@ -97,32 +97,32 @@ def main(_):
 
 
 	sess = tf.InteractiveSession()
-#	tf.global_variables_initializer().run()
-#	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-#	train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-###  Train
-#	for _ in range(1000):
-#		batch_xs, batch_ys = mnist.train.next_batch(100)
-#		sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+	tf.global_variables_initializer().run()
+	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+	train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+##  Train
+	for _ in range(1000):
+		batch_xs, batch_ys = mnist.train.next_batch(100)
+		sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
-	train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-	correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-	sess.run(tf.global_variables_initializer())
-	for i in range(20000):
-		batch = mnist.train.next_batch(50)
-		if i%100 == 0:
-			train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
-			print("step %d, training accuracy %g"%(i, train_accuracy))
-		train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-	print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+#	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
+#	train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+#	correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+#	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+#	sess.run(tf.global_variables_initializer())
+#	for i in range(20000):
+#		batch = mnist.train.next_batch(50)
+#		if i%100 == 0:
+#			train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
+#			print("step %d, training accuracy %g"%(i, train_accuracy))
+#		train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+#
+#	print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
 # Test trained model
-#  correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-#  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-#  print(sess.run(accuracy, feed_dict={x: mnist.test.images,y_: mnist.test.labels}))
+	correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	print(sess.run(accuracy, feed_dict={x: mnist.test.images,y_: mnist.test.labels}))
 
 	sess.close()
 if __name__ == '__main__':
